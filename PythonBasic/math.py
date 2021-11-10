a4778c0710910af418cede5c5b84fb4712e31e03
SeraWi <wsr2357@naver.com>
2021-11-10 21:00:08 +0900

    Python Basic : math 함수

---
+++ PythonBasic/math.py
@@ -0,0 +1,14 @@
+print(abs(-5)) #절댓값
+print(pow(4,2)) # 4^2 = 16  승
+print(max(5,12))
+print(min(5,12))
+print(round(3.14)) #반올림 3
+print(round(4.99)) # 5
+
+
+from math import *
+print(floor(4.99)) #내림 4
+print(ceil(3.14)) #올림 4
+print(sqrt(16)) #제곱근 4
+
+
