a96a4e118458d5a3174fda361362eef50214ecb9
SeraWi <wsr2357@naver.com>
2021-11-10 12:40:08 +0900

    Create practice.py

---
+++ PythonBasic/practice.py
@@ -0,0 +1,41 @@
+# 숫자
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(5*3)
+print(3*(3+1))
+
+# 문자 '', "" 가능
+print('풍선')
+print("나비")
+print("zzzzzzzz")
+print("ㅋ"*9)
+
+#boorlan 참/거짓
+print(5>10) #false
+print(5<10)
+print(True)
+print(not True) #false
+print(not False) 
+
+# 애완동물을 소개해 주세요
+#변수 : 값 저장 공간
+animal ="강아지"
+name ="연탄이"
+age = 4
+hobby ="산책"
+is_adult = age>=3
+
+# 정수형 출력할 떄 str(숫자)
+
+print("우리집 "+ animal+"의 이름은 "+name+"에요")
+hobby ="공놀이"
+#print(name+"는 "+ str(age)+"살이며, "+hobby+"을 아주 좋아해요")
+print(name,"는 ", age,"살이며, ",hobby, "을 아주 좋아해요")
+print(name+"는 어른일까요? "+str(is_adult))
+
+
+#주석22: 27
+
