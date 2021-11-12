49388b567f5306f4b13ce6cf9717b9c1d89c82db
SeraWi <wsr2357@naver.com>
2021-11-12 13:41:26 +0900

    PythonBasic : tuple/ 튜플

---
+++ PythonBasic/tuple.py
@@ -0,0 +1,19 @@
+# 튜플
+# 리스트와 달리 내용을 변경, 추가 할 수 없다.
+# 속도가 리스트보다 빠르다
+# ()
+
+
+menu =("돈까스","치즈까스")
+print(menu[0])
+print(menu[1])
+
+#menu.add("생선까스") 추가 불가능
+
+name ="김종국"
+age ="20"
+hobby ="코딩"
+print(name,age, hobby)
+
+(name, age, hobby) =("김종국", 20, "코딩")
+print(name, age, hobby)
