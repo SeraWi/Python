53cb204cd764b5142d9f1fbbd100dda616c0dffe
SeraWi <wsr2357@naver.com>
2021-11-15 12:23:04 +0900

    PythonBasic: dictionary

--- PythonBasic/dictionary.py
+++ PythonBasic/dictionary.py
@@ -13,7 +13,7 @@ print(cabinet.get(5)) #key 값 없으면 None 출력
 print(cabinet.get(5,"사용가능")) # key =5 가 있으면 value 반환, 없으면 사용가능 반환
 
 print(3 in cabinet) # key값이 존재하는지 반환 True
-print(5 in cabinet) # Fale
+print(5 in cabinet) # False
 
 print(cabinet["A"]) #key 값 string 도 가능
 
